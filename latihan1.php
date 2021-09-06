45745b0a4dd3637ad7e2e600dd20b92e99b5baa3
Renshinnn <buzworry@gmail.com>
2021-09-06 16:36:58 +0700

    PHP 1

---
+++ latihan1.php
@@ -0,0 +1,3 @@
+<?php
+    echo 'Namaku Khiky'.'<br>'.'Aku sedang belajar PHP di SMK Telkom Malang'
+?>
---
+++ latihan5a.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Variabel antar Halaman</title>
+    </head>
+    <body>
+        <form action = "variabel_hasil.php" method = "POST">
+            Nama : <input type="text" name="nama"> <br>
+            TTL : <input type = "date" name="ttl"><br>
+            Email : <input type = "text" name ="email"> <br>
+            <input type = "submit" name ="submit" value = "Tampilkan">
+
+        </form>
+    </body>
+</html>
